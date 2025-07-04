8ab3a23df880f23f904cf72db18215de5a8b108e
GabrielDesenvolver777 <gabrielalexok@gmail.com>
2025-07-04 18:08:33 -0300

    DevBurguer API

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,17 +1,16 @@
-import multer from "multer";
-import { extname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { v4 } from "uuid";
+import multer from 'multer';
+import { dirname, extname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { v4 } from 'uuid';
 
-// Obter o diretório atual
+// Simula o __dirname em ES Modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = resolve(__filename, "..", "..");
+const __dirname = dirname(__filename);
 
 export default {
-	storage: multer.diskStorage({
-		destination: resolve(__dirname, "uploads"),
-		filename: (request, file, callback) => {
-			return callback(null, v4() + extname(file.originalname));
-		},
-	}),
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, callback) =>
+      callback(null, v4() + extname(file.originalname)),
+  }),
 };
